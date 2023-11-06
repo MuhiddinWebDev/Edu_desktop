361b267466bd1ad8ca685cd38ec6063a48f66d2f
MuhiddinWebDev <xazratqulovmuhiddin@gmail.com>
2023-11-06 10:57:35 +0500

    fixed index

--- dist-electron/main/index.js
+++ dist-electron/main/index.js
@@ -1,1 +1,150 @@
-"use strict";const e=require("electron"),g=require("node:os"),i=require("node:path"),a=require("electron-updater");a.autoUpdater.autoDownload=!0;a.autoUpdater.setFeedURL({provider:"github",owner:"MuhiddinWebDev",repo:"frontend",private:!1,vPrefixedTagName:!0});process.env.DIST_ELECTRON=i.join(__dirname,"..");process.env.DIST=i.join(process.env.DIST_ELECTRON,"../dist");process.env.VITE_PUBLIC=process.env.VITE_DEV_SERVER_URL?i.join(process.env.DIST_ELECTRON,"../public"):process.env.DIST;g.release().startsWith("6.1")&&e.app.disableHardwareAcceleration();process.platform==="win32"&&e.app.setAppUserModelId(e.app.getName());e.app.requestSingleInstanceLock()||(e.app.quit(),process.exit(0));let o=null;const d=i.join(__dirname,"../preload/index.js"),p=process.env.VITE_DEV_SERVER_URL,c=i.join(process.env.DIST,"index.html");async function u(){const{width:t,height:s}=e.screen.getPrimaryDisplay().workAreaSize;o=new e.BrowserWindow({title:"Main window",icon:i.join(process.env.VITE_PUBLIC,"logo.png"),width:t,height:s,webPreferences:{preload:d,nodeIntegration:!0,contextIsolation:!0,sandbox:!0,webSecurity:!0,devTools:!0},autoHideMenuBar:!0}),process.env.VITE_DEV_SERVER_URL?(o.loadURL(p),o.webContents.openDevTools()):o.loadFile(c),o.webContents.on("did-finish-load",()=>{o==null||o.webContents.send("main-process-message",new Date().toLocaleString())}),o.webContents.setWindowOpenHandler(({url:r})=>(r.startsWith("https:")&&e.shell.openExternal(r),{action:"deny"}));let n=!1;o.on("close",r=>{if(!n){r.preventDefault();const w={type:"question",buttons:["Chiqish","Bekor qilish"],defaultId:0,title:"Ilovadan chiqing",message:"Haqiqatan ham ilovadan chiqmoqchimisiz?",cancelId:5};e.dialog.showMessageBox(o,w).then(l=>{console.log(l.response),l.response===0?(n=!0,e.app.quit()):n=!1})}})}e.app.whenReady().then(u);e.app.on("window-all-closed",()=>{o=null,process.platform!=="darwin"&&e.app.quit()});e.app.on("second-instance",()=>{o&&(o.isMinimized()&&o.restore(),o.focus())});e.app.on("activate",()=>{const t=e.BrowserWindow.getAllWindows();t.length?t[0].focus():u()});e.ipcMain.handle("open-win",(t,s)=>{const n=new e.BrowserWindow({webPreferences:{preload:d,nodeIntegration:!0,contextIsolation:!1}});console.log(process.env),process.env.VITE_DEV_SERVER_URL?n.loadURL(`${p}#${s}`):n.loadFile(c,{hash:s})});e.app.on("browser-window-focus",()=>{e.globalShortcut.register("CommandOrControl+R",()=>{console.log("CommandOrControl+R is pressed: Shortcut Disabled")})});e.app.on("browser-window-blur",function(){e.globalShortcut.unregister("CommandOrControl+R")});e.app.commandLine.appendSwitch("ignore-certificate-errors");e.app.on("ready",async()=>{await a.autoUpdater.checkForUpdates()});a.autoUpdater.on("update-downloaded",async t=>{const s={type:"question",buttons:["Yang"],title:"Eng so'nggi versiya yuklangan",message:"Eng so'nggi versiya yuklangan",defaultId:0,cancelId:0};e.dialog.showMessageBox(o,s).then(n=>{n.response===0&&a.autoUpdater.quitAndInstall()})});e.app.commandLine.appendSwitch("ignore-certificate-errors","true");
+"use strict";
+const electron = require("electron");
+const node_os = require("node:os");
+const node_path = require("node:path");
+const electronUpdater = require("electron-updater");
+electronUpdater.autoUpdater.autoDownload = true;
+electronUpdater.autoUpdater.setFeedURL({
+  provider: "github",
+  owner: "MuhiddinWebDev",
+  repo: "frontend",
+  private: false,
+  vPrefixedTagName: true
+});
+process.env.DIST_ELECTRON = node_path.join(__dirname, "..");
+process.env.DIST = node_path.join(process.env.DIST_ELECTRON, "../dist");
+process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL ? node_path.join(process.env.DIST_ELECTRON, "../public") : process.env.DIST;
+if (node_os.release().startsWith("6.1"))
+  electron.app.disableHardwareAcceleration();
+if (process.platform === "win32")
+  electron.app.setAppUserModelId(electron.app.getName());
+if (!electron.app.requestSingleInstanceLock()) {
+  electron.app.quit();
+  process.exit(0);
+}
+let win = null;
+const preload = node_path.join(__dirname, "../preload/index.js");
+const url = process.env.VITE_DEV_SERVER_URL;
+const indexHtml = node_path.join(process.env.DIST, "index.html");
+async function createWindow() {
+  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
+  win = new electron.BrowserWindow({
+    title: "Main window",
+    icon: node_path.join(process.env.VITE_PUBLIC, "logo.png"),
+    width,
+    height,
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      // Disable Node.js integration
+      contextIsolation: true,
+      // Enable context isolation
+      sandbox: true,
+      webSecurity: true,
+      devTools: true
+    },
+    autoHideMenuBar: true
+  });
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(url);
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(indexHtml);
+  }
+  win.webContents.on("did-finish-load", () => {
+    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+  });
+  win.webContents.setWindowOpenHandler(({ url: url2 }) => {
+    if (url2.startsWith("https:"))
+      electron.shell.openExternal(url2);
+    return { action: "deny" };
+  });
+  let closeboll = false;
+  win.on("close", (e) => {
+    if (!closeboll) {
+      e.preventDefault();
+      const options = {
+        type: "question",
+        buttons: ["Chiqish", "Bekor qilish"],
+        defaultId: 0,
+        title: "Ilovadan chiqing",
+        message: "Haqiqatan ham ilovadan chiqmoqchimisiz?",
+        cancelId: 5
+      };
+      electron.dialog.showMessageBox(win, options).then((response) => {
+        console.log(response.response);
+        if (response.response === 0) {
+          closeboll = true;
+          electron.app.quit();
+        } else {
+          closeboll = false;
+        }
+      });
+    }
+  });
+}
+electron.app.whenReady().then(createWindow);
+electron.app.on("window-all-closed", () => {
+  win = null;
+  if (process.platform !== "darwin")
+    electron.app.quit();
+});
+electron.app.on("second-instance", () => {
+  if (win) {
+    if (win.isMinimized())
+      win.restore();
+    win.focus();
+  }
+});
+electron.app.on("activate", () => {
+  const allWindows = electron.BrowserWindow.getAllWindows();
+  if (allWindows.length) {
+    allWindows[0].focus();
+  } else {
+    createWindow();
+  }
+});
+electron.ipcMain.handle("open-win", (_, arg) => {
+  const childWindow = new electron.BrowserWindow({
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  console.log(process.env);
+  if (process.env.VITE_DEV_SERVER_URL) {
+    childWindow.loadURL(`${url}#${arg}`);
+  } else {
+    childWindow.loadFile(indexHtml, { hash: arg });
+  }
+});
+electron.app.on("browser-window-focus", () => {
+  electron.globalShortcut.register("CommandOrControl+R", () => {
+    console.log("CommandOrControl+R is pressed: Shortcut Disabled");
+  });
+});
+electron.app.on("browser-window-blur", function() {
+  electron.globalShortcut.unregister("CommandOrControl+R");
+});
+electron.app.commandLine.appendSwitch("ignore-certificate-errors");
+electron.app.on("ready", async () => {
+  await electronUpdater.autoUpdater.checkForUpdates();
+});
+electronUpdater.autoUpdater.on("update-downloaded", async (info) => {
+  const options = {
+    type: "question",
+    buttons: ["Yang"],
+    title: "Eng so'nggi versiya yuklangan",
+    message: "Eng so'nggi versiya yuklangan",
+    defaultId: 0,
+    cancelId: 0
+  };
+  electron.dialog.showMessageBox(win, options).then((response) => {
+    if (response.response === 0) {
+      electronUpdater.autoUpdater.quitAndInstall();
+    }
+  });
+});
+electron.app.commandLine.appendSwitch("ignore-certificate-errors", "true");
+//# sourceMappingURL=index.js.map
